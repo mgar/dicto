75d8b34277da215a2333b08114734ce0f662d508
Miguel Ángel García <mgarcia.inf@gmail.com>
2026-03-24 17:32:53 -0700

    feat: run gh workflow for testing backend

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,7 +2,17 @@
 API Routers package.
 """
 from .auth import router as auth_router
+from .grammar import router as grammar_router
+from .learn import router as learn_router
+from .reviews import router as reviews_router
+from .stats import router as stats_router
+from .vocab import router as vocab_router
 
 __all__ = [
     "auth_router",
+    "grammar_router",
+    "learn_router",
+    "reviews_router",
+    "stats_router",
+    "vocab_router",
 ]
